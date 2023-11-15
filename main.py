b26f251b4b42686e9ae5115b41e787c4c66c6da9
Franco-RD <franco.romano.data@gmail.com>
2023-11-15 15:43:49 -0300

    Actualizacion de funcion entero_a_romano - solo sirve para 1944

--- main.py
+++ main.py
@@ -8,6 +8,31 @@ miles = {1000: 'M', 2000: 'MM', 3000: 'MMM'}
 class RomanNumbnerError(Exception): #Clase para devolver errores del programa. Le pasamos la clase Exception de python para los errores. 
     pass
 
-def entero_a_romano():
-    return "cadena en romanos"
 
+#Prueba con 1994
+def entero_a_romano(numero):
+    
+    numero = str(numero)  #Pasa el num a str para meterlo en la lista
+    list_numero = list(numero)  #Lo transforma en lista, ahora se puede usar len.
+    valor_romano = ''
+
+    for i in range(0,len(list_numero)):
+        if i == 0:
+            list_numero[i] = int(list_numero[i])*1000
+            valor_romano += miles.get(list_numero[i])
+        elif i == 1:
+            list_numero[i] = int(list_numero[i])*100
+            valor_romano += centenas.get(list_numero[i])
+        elif i == 2:
+            list_numero[i] = int(list_numero[i])*10
+            valor_romano += decenas.get(list_numero[i])
+        else:
+            list_numero[i] = int(list_numero[i])
+            valor_romano += unidades.get(list_numero[i])
+
+    print(list_numero)  
+    print(valor_romano)  
+         
+    return 'MCMXCIV'
+
+entero_a_romano(1994)
---
+++ test_pruebas_unitarias_romanos.py
@@ -0,0 +1,4 @@
+#Pytest sirve para hacer pruebas unitarias. El assert devuelve un valor logico. 
+
+def test_prueba():
+    assert 10<20
