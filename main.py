429290fa339f0ba04630617657be7364ce1e5b3c
Franco-RD <franco.romano.data@gmail.com>
2023-11-15 16:25:52 -0300

    Funcion entero_a_romano cambiada para que funcione con while

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ decenas = {10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX',
 centenas = {100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'CCM', 900: 'CM'}
 miles = {1000: 'M', 2000: 'MM', 3000: 'MMM'}
 
+dic_completo = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'IIX', 9: 'IX',
+                10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'XXC', 90: 'XC',
+                100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'CCM', 900: 'CM',
+                1000: 'M', 2000: 'MM', 3000: 'MMM'}
+
 class RomanNumbnerError(Exception): #Clase para devolver errores del programa. Le pasamos la clase Exception de python para los errores. 
     pass
 
@@ -16,6 +21,16 @@ def entero_a_romano(numero):
     list_numero = list(numero)  #Lo transforma en lista, ahora se puede usar len.
     valor_romano = ''
 
+    cont = 0  #Para el while
+    valor_num = 1000  #Para el while
+
+    while cont < 4:     #solucionar que no funciona para menos de 1000
+        list_numero[cont] = int(list_numero[cont])*valor_num
+        valor_romano += dic_completo.get(list_numero[cont])
+        cont += 1
+        valor_num = valor_num/10 
+
+    '''
     for i in range(0,len(list_numero)):
         if i == 0:
             list_numero[i] = int(list_numero[i])*1000
@@ -29,8 +44,9 @@ def entero_a_romano(numero):
         else:
             list_numero[i] = int(list_numero[i])
             valor_romano += unidades.get(list_numero[i])
+    '''
 
     return valor_romano
 
 
-entero_a_romano(1994)
+print(entero_a_romano(1990))
