cccce98cacdf79045dc2e7d25e47522de44fdb9a
Franco-RD <franco.romano.data@gmail.com>
2023-11-15 14:13:24 -0300

    Primera funcion entero a romano

--- pruebas.py
+++ pruebas.py
@@ -5,9 +5,3 @@ decenas = {10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX',
 centenas = {100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'CCM', 900: 'CM'}
 miles = {1000: 'M', 2000: 'MM', 3000: 'MMM'}
 
-class RomanNumbnerError(Exception): #Clase para devolver errores del programa. Le pasamos la clase Exception de python para los errores. 
-    pass
-
-def entero_a_romano():
-    return "cadena en romanos"
-
