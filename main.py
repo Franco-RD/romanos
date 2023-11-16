1f42767044f77411c961b77f7caf45ba1af8c96e
Franco-RD <franco.romano.data@gmail.com>
2023-11-16 16:56:12 -0300

    Notas de funcionamiento y clase agregadas

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 dic_romanos = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 
-unidades = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
-decenas = {10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC'}
-centenas = {100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCCC', 800: 'CCM', 900: 'CM'}
-miles = {1000: 'M', 2000: 'MM', 3000: 'MMM'}
-
 dic_completo = {0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
                 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
                 100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
@@ -14,9 +9,10 @@ class RomanNumbnerError(Exception): #Clase para devolver errores del programa. L
     pass
 
 
-#Prueba con 1994
 def entero_a_romano(numero):
     
+    #numero = "{:0>4d}".format(numero)  #En vez de hacer la cuenta en valor_num, con esto siempre hago que mi numero tenga 4 digitos, y puedo dejar valor_num = 1000. Ademas como es un str me ahorro la linea que sigue
+
     numero = str(numero)  #Pasa el num a str para meterlo en la lista
     list_numero = list(numero)  #Lo transforma en lista, ahora se puede usar len. Ej 1940 [1, 9, 4, 0]
     valor_romano = ''  #str para guardar el numero en romano
@@ -26,8 +22,8 @@ def entero_a_romano(numero):
 
     while cont < len(list_numero):     
         list_numero[cont] = int(list_numero[cont])*valor_num
-        valor_romano += dic_completo.get(list_numero[cont])
-        cont += 1
+        valor_romano += dic_completo.get(list_numero[cont])  #Se puede poner una coma en dic_completo.get(list_numero[cont], '') En este caso al no encontrar la clave el get me va a devolver ''. Esto serviria para sacar el 0 del diccionario
+        cont += 1           
         valor_num = valor_num/10 
 
     '''
--- test_pruebas_unitarias_romanos.py
+++ test_pruebas_unitarias_romanos.py
@@ -4,25 +4,14 @@
 
 from main import entero_a_romano
 
-valor_romano4 = entero_a_romano(1994)
-
 def test_prueba_entero_a_romano4():
-    assert valor_romano4 == 'MCMXCIV'
-
-
-valor_romano3 = entero_a_romano(837)
+    assert entero_a_romano(1994) == 'MCMXCIV'
 
 def test_prueba_entero_a_romano3():
-    assert valor_romano3 == 'DCCCXXXVII'
-
-
-valor_romano2 = entero_a_romano(64)
+    assert entero_a_romano(837) == 'DCCCXXXVII'
 
 def test_prueba_entero_a_romano2():
-    assert valor_romano2 == 'LXIV'
-
-
-valor_romano1 = entero_a_romano(7)
+    assert entero_a_romano(64) == 'LXIV'
 
 def test_prueba_entero_a_romano1():
-    assert valor_romano1 == 'VII'
+    assert entero_a_romano(7) == 'VII'
