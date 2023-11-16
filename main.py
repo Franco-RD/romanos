1cb7e8bb97dd30355a567633fded77d5a68ec383
Franco-RD <franco.romano.data@gmail.com>
2023-11-16 14:08:18 -0300

    Arreglada funcion entero_a_romano para que funcione con 1, 2 y 3 digitos tambien

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 dic_romanos = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 
-unidades = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'IIX', 9: 'IX'}
-decenas = {10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'XXC', 90: 'XC'}
-centenas = {100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'CCM', 900: 'CM'}
+unidades = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
+decenas = {10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC'}
+centenas = {100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCCC', 800: 'CCM', 900: 'CM'}
 miles = {1000: 'M', 2000: 'MM', 3000: 'MMM'}
 
-dic_completo = {0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'IIX', 9: 'IX',
-                10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'XXC', 90: 'XC',
-                100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'CCM', 900: 'CM',
+dic_completo = {0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+                10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
+                100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
                 1000: 'M', 2000: 'MM', 3000: 'MMM'}
 
 class RomanNumbnerError(Exception): #Clase para devolver errores del programa. Le pasamos la clase Exception de python para los errores. 
@@ -18,13 +18,13 @@ class RomanNumbnerError(Exception): #Clase para devolver errores del programa. L
 def entero_a_romano(numero):
     
     numero = str(numero)  #Pasa el num a str para meterlo en la lista
-    list_numero = list(numero)  #Lo transforma en lista, ahora se puede usar len.
-    valor_romano = ''
+    list_numero = list(numero)  #Lo transforma en lista, ahora se puede usar len. Ej 1940 [1, 9, 4, 0]
+    valor_romano = ''  #str para guardar el numero en romano
 
     cont = 0  #Para el while
-    valor_num = 1000  #Para el while
+    valor_num = int(10 ** (len(list_numero)-1))  #Para el while. Multiplicador para cada elemento del numero en la lista. 
 
-    while cont < 4:     #solucionar que no funciona para menos de 1000
+    while cont < len(list_numero):     
         list_numero[cont] = int(list_numero[cont])*valor_num
         valor_romano += dic_completo.get(list_numero[cont])
         cont += 1
@@ -49,4 +49,4 @@ def entero_a_romano(numero):
     return valor_romano
 
 
-print(entero_a_romano(1900))
+print(entero_a_romano(1994))
--- test_pruebas_unitarias_romanos.py
+++ test_pruebas_unitarias_romanos.py
@@ -4,9 +4,25 @@
 
 from main import entero_a_romano
 
-valor_romano = entero_a_romano(1994)
+valor_romano4 = entero_a_romano(1994)
 
-def test_prueba_entero_a_romano():
-    assert valor_romano == 'MCMXCIV'
+def test_prueba_entero_a_romano4():
+    assert valor_romano4 == 'MCMXCIV'
 
-test_prueba_entero_a_romano()
+
+valor_romano3 = entero_a_romano(837)
+
+def test_prueba_entero_a_romano3():
+    assert valor_romano3 == 'DCCCXXXVII'
+
+
+valor_romano2 = entero_a_romano(64)
+
+def test_prueba_entero_a_romano2():
+    assert valor_romano2 == 'LXIV'
+
+
+valor_romano1 = entero_a_romano(7)
+
+def test_prueba_entero_a_romano1():
+    assert valor_romano1 == 'VII'
